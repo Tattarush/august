91493e65cbe4d256e279f9322ee3af9c8e9873c0
Tattarush <tattarush@gmail.com>
2026-08-16 19:25:09 +0300

    day 3 double

---
+++ src/main/java/day16/four/Main.java
@@ -0,0 +1,14 @@
+package day16.four;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double x, y, z;
+        x = 3;
+        y = 4;
+
+        z = Math.sqrt(x * x + y * y);
+        System.out.println("Квадрат гипотенузы равен " + z);
+
+    }
+}
