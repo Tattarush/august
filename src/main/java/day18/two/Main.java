3b507b87af288d1a319ff3e90d8cb193c0d7093b
Tattarush <tattarush@gmail.com>
2026-08-18 17:23:00 +0300

    day 4 boolean 2

---
+++ src/main/java/day18/two/Main.java
@@ -0,0 +1,14 @@
+package day18.two;
+
+/*Рассчитать расстояние до места вспЬП11ки молнии, звук от которого
+доходит до наблюдателя через 7,2 секунды.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        double distancePerSec = 1100;
+        double distance = distancePerSec * 7.2;
+
+        System.out.println(distance);
+    }
+}
