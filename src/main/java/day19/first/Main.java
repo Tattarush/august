14e6c34b995cbfa31932f4ed90997b2eae07d8f8
Tattarush <tattarush@gmail.com>
2026-08-19 12:36:58 +0300

    day 5 спецсимволы

---
+++ src/main/java/day19/first/Main.java
@@ -0,0 +1,12 @@
+package day19.first;
+
+/*спецсимволы часть*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Первая строка\nВторая строка");
+        System.out.println("a\tb\tc");
+        System.out.println("d\te\tf");
+    }
+}
