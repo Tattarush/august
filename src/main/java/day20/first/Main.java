575bd9f5ee53b2679aca8bff0c2934702b924718
Tattarush <tattarush@gmail.com>
2026-08-20 16:46:10 +0300

    day 6 del

---
+++ src/main/java/day20/first/Main.java
@@ -0,0 +1,19 @@
+package day20.first;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        double z = 10.0;
+        double f = 3.0;
+        System.out.println(z / f);
+        System.out.println(z % f);
+
+    }
+}
