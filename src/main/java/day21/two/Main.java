59707bf6225c6dce43d345773a80acbef616f89c
Tattarush <tattarush@gmail.com>
2026-08-21 17:09:49 +0300

    day 7 логические операции

---
+++ src/main/java/day21/two/Main.java
@@ -0,0 +1,9 @@
+package day21.two;
+
+public class Main {
+    public static void main(String[] args) {
+        int x , y, z;
+        x = y = z = 100;
+        System.out.println(x + " "+ y +" "+ z);
+    }
+}
---
+++ src/main/java/day21/two/Solution.java
@@ -0,0 +1,19 @@
+package day21.two;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int a = 10;
+        float b;
+
+        b = a;
+
+        System.out.println(b);
+
+
+        double x = 10.0 , y = 4.0;
+
+        System.out.println((int) (x / y));
+
+    }
+}
