3af600c7464d2bbce545fe8bd4aaf54105dc624a
Tattarush <tattarush@gmail.com>
2026-08-23 20:47:56 +0300

    day 9 простые числа

---
+++ src/main/java/day23/first/Main.java
@@ -0,0 +1,23 @@
+package day23.first;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i <= 100; i++) {
+            int count = 0;
+            for (int j = 2; j <= 100; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count <= 1) {
+                list.add(i);
+            }
+        }
+        System.out.println(list);
+    }
+}
