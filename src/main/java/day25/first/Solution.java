9c09eb889ee8903f35554054f327f2adb9abe93c
Tattarush <tattarush@gmail.com>
2026-08-25 11:47:21 +0300

    day 10 оператор for считывание посимвольно метод skip b available

---
+++ src/main/java/day25/first/Solution.java
@@ -0,0 +1,14 @@
+package day25.first;
+
+public class Solution {
+    public static void main(String[] args) throws java.io.IOException {
+
+
+        System.out.println("Введите S для выхода из цикла");
+        for (int i = 0; (char) System.in.read() != 'S'; i++) {
+            System.out.println("Проход номер " + i);
+            System.in.skip(System.in.available());
+        }
+
+    }
+}
