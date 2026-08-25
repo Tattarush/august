f3c6eb9d288af15a412b0368212f74a9f3344fd9
Tattarush <tattarush@gmail.com>
2026-08-25 11:27:21 +0300

    day 10 оператор for

---
+++ src/main/java/day25/first/Main.java
@@ -0,0 +1,11 @@
+package day25.first;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i, j;
+        for (i = 0, j = 10; i < j; i++, j--) {
+            System.out.println(i+" "+j);
+        }
+    }
+}
