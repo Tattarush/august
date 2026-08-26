3a0f1f544441deaefda452edec6f0b475d78d788
Tattarush <tattarush@gmail.com>
2026-08-26 23:25:45 +0300

    day 10 оператор break метки блоков

---
+++ src/main/java/day26/first/Solution2.java
@@ -0,0 +1,24 @@
+package day26.first;
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 3; i++) {
+            one: {
+                two:{
+                    three:{
+                        System.out.println("i - равно = " + i);
+                        if(i == 0) break one;
+                        if(i == 1) break two;
+                        if(i == 2) break three;
+                        System.out.println("three");
+                    }
+                    System.out.println("two");
+                }
+                System.out.println("one");
+            }
+        }
+        System.out.println("Finish");
+    }
+}
