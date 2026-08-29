35ee700109feb3c3cbb0583369d44ab29fec78a3
Tattarush <tattarush@gmail.com>
2026-08-29 16:01:27 +0300

    day 12 for

---
+++ src/main/java/day28/two/Solution.java
@@ -0,0 +1,12 @@
+package day28.two;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        for (int i = 1000; i >= 0 ; i -= 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
