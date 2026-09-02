e13d28270139dea1df7885f62d68c8dd9b49bb6a
Tattarush <tattarush@gmail.com>
2026-09-02 21:36:46 +0300

    Классы база

--- src/main/java/september/day02/first/Main.java
+++ src/main/java/september/day02/first/Main.java
@@ -3,7 +3,7 @@ package september.day02.first;
 public class Main {
     public static void main(String[] args) {
 
-        int range;
+        int range1 , range2;
 
         Vehicle minivan = new Vehicle();
 
@@ -11,9 +11,19 @@ public class Main {
         minivan.fuelcap = 16;
         minivan.mpg = 21;
 
-        range = minivan.fuelcap * minivan.mpg;
+        range1 = minivan.fuelcap * minivan.mpg;
 
-        System.out.println("Минивен может перевезти "+ minivan.passengers+ " пассажиров на "+ range + " миль");
+        Vehicle sportcar = new Vehicle();
+
+        sportcar.passengers = 2;
+        sportcar.fuelcap = 14;
+        sportcar.mpg = 12;
+
+        range2 = sportcar.fuelcap * sportcar.mpg;
+
+        System.out.println("Минивен может перевезти " + minivan.passengers + " пассажиров на " + range1 + " миль");
+        System.out.println("Спорткар может перевезти " + sportcar.passengers + " пассажиров на " + range2 + " миль");
+
     }
 }
 
