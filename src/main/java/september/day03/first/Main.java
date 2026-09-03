b0f161f75b9937ce866f5a0a39d74d987eeb960e
Tattarush <tattarush@gmail.com>
2026-09-03 20:53:44 +0300

    Классы, конструкторы, параметризованные конструкторы

---
+++ src/main/java/september/day03/first/Main.java
@@ -0,0 +1,17 @@
+package september.day03.first;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pwr x = new Pwr(4.0, 2);
+        Pwr y = new Pwr(2.5, 1);
+        Pwr z = new Pwr(5.7, 0);
+
+
+        System.out.println(x.getPwr());
+        System.out.println(y.getPwr());
+        System.out.println(z.getPwr());
+
+
+    }
+}
