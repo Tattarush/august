011d2502c0e5b28cf04b99f0ef4855810d6b68f7
Tattarush <tattarush@gmail.com>
2026-09-05 15:48:20 +0300

    args

---
+++ src/main/java/september/day05/two/Main.java
@@ -0,0 +1,13 @@
+package september.day05.two;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+    public static void main(String @NotNull [] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+    }
+}
