d264249b81300d9b75ad3446d7bfb8804860cc29
Tattarush <tattarush@gmail.com>
2026-09-05 16:07:36 +0300

    побитовые операции

---
+++ src/main/java/september/day05/four/Main.java
@@ -0,0 +1,15 @@
+package september.day05.four;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char ch;
+
+        for (int i = 0; i < 10; i++) {
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+            ch = (char) ((int) ch & 65503);
+            System.out.print(ch+" ");
+        }
+    }
+}
