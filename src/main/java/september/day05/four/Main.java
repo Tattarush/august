ca23628e6f73f07c8ec9955399132d623cf000d8
Tattarush <tattarush@gmail.com>
2026-09-05 16:22:14 +0300

    побитовые операции

--- src/main/java/september/day05/four/Main.java
+++ src/main/java/september/day05/four/Main.java
@@ -11,5 +11,14 @@ public class Main {
             ch = (char) ((int) ch & 65503);
             System.out.print(ch+" ");
         }
+
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            ch = (char) ('A' + i);
+            System.out.print(ch);
+            ch = (char) ((int) ch | 32);
+            System.out.print(ch + " ");
+        }
     }
 }
